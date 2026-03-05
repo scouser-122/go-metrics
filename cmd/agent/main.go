067a7292133fa6ba27cd38611446e4a4a4fa1966
scouser-122 <scouser122@gmail.com>
2026-03-05 11:30:48 +0300

    fix: autotest errors

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -9,18 +9,18 @@ import (
 
 func main() {
 	config := agent.GetDefaultAgentConfig()
-	initFlags(&config)
-	flag.Parse()
+	parseFlags(&config)
 	agent := agent.RuntimeMetricsAgent{
 		Config: config,
 	}
 	agent.CollectAndSendMetricsInLooop()
 }
 
-func initFlags(agentConfig *agent.AgentConfig) {
+func parseFlags(agentConfig *agent.AgentConfig) {
 	serverAddress := ""
 	flag.StringVar(&serverAddress, "a", "localhost:8080", "server address in format host:port")
-	agentConfig.ServerAddress = fmt.Sprintf("http://%s", serverAddress)
 	flag.IntVar(&agentConfig.ReportInterval, "r", 10, "metrics report interval in seconds")
 	flag.IntVar(&agentConfig.PollInterval, "p", 2, "metrics poll interval in seconds")
+	flag.Parse()
+	agentConfig.ServerAddress = fmt.Sprintf("http://%s", serverAddress)
 }
