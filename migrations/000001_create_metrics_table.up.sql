cd6e693f26de94c78ee714142b71ba3bf49543f2
scouser-122 <scouser122@gmail.com>
2026-04-06 10:57:49 +0300

    fix: autotests errors

--- migrations/000001_create_metrics_table.up.sql
+++ migrations/000001_create_metrics_table.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE metrics (
     id VARCHAR(255) NOT NULL,
     type VARCHAR(7) NOT NULL,
-    delta INT NULL,
+    delta BIGINT NULL,
     value DOUBLE PRECISION NULL,
     hash VARCHAR(511) NULL,
     PRIMARY KEY(id, type)
