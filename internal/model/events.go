b554cce7283822959bc945b9b7f148fa1952285a
scouser-122 <scouser122@gmail.com>
2026-06-16 19:27:48 +0300

    fix: async audit events storage

--- internal/model/events.go
+++ internal/model/events.go
@@ -2,13 +2,8 @@ package models
 
 import "github.com/botchris/go-pubsub"
 
-// MetricEvent is a marker interface for metric-related events.
-type MetricEvent interface{}
-
 // MetricsReceivedEvent metric received event.
 type MetricsReceivedEvent struct {
-	MetricEvent `json:"-"`
-
 	// TS event's unix timestamp.
 	TS int64 `json:"ts"`
 
--- internal/service/audit.go
+++ internal/service/audit.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/botchris/go-pubsub"
 	"github.com/go-resty/resty/v2"
@@ -30,11 +31,9 @@ func NewAuditService(serverConfig *config.ServerConfig) AuditService {
 // SubscribeToMetricEvents subscribes to metric receieve events, in case of error - just log.
 func (service *AuditService) SubscribeToMetricEvents(eventBroker pubsub.Broker, ctx context.Context) {
 	if service.serverConfig.AuditFile != "" {
-		handler := pubsub.NewHandler(func(ctx context.Context, topic pubsub.Topic, event models.MetricEvent) error {
-			err := service.LogMetricEventToFile(event)
-			if err != nil {
-				logger.Sugar.Errorf("metrics event save to file error: %w", err)
-			}
+		fileSaveEvents := make(chan models.MetricsReceivedEvent, 50)
+		handler := pubsub.NewHandler(func(ctx context.Context, topic pubsub.Topic, event models.MetricsReceivedEvent) error {
+			fileSaveEvents <- event
 			return nil
 		})
 		sub, err := eventBroker.Subscribe(ctx, models.MetricEventTopic, handler)
@@ -48,14 +47,13 @@ func (service *AuditService) SubscribeToMetricEvents(eventBroker pubsub.Broker, 
 				}
 			}()
 			logger.Sugar.Info("file-system audit start listen to metrics event")
+			go logMetricEventsToFile(fileSaveEvents, service.serverConfig.AuditFile)
 		}
 	}
 	if service.serverConfig.AuditURL != "" {
-		handler := pubsub.NewHandler(func(ctx context.Context, topic pubsub.Topic, event models.MetricEvent) error {
-			err := service.LogMetricEventToRemoteService(event)
-			if err != nil {
-				logger.Sugar.Errorf("metrics event send to remote service error: %w", err)
-			}
+		serviceSendEvents := make(chan models.MetricsReceivedEvent, 50)
+		handler := pubsub.NewHandler(func(ctx context.Context, topic pubsub.Topic, event models.MetricsReceivedEvent) error {
+			serviceSendEvents <- event
 			return nil
 		})
 		sub, err := eventBroker.Subscribe(ctx, models.MetricEventTopic, handler)
@@ -69,44 +67,54 @@ func (service *AuditService) SubscribeToMetricEvents(eventBroker pubsub.Broker, 
 				}
 			}()
 			logger.Sugar.Info("remote service audit start listen to metrics event")
+			go logMetricEventToRemoteService(serviceSendEvents, service.serverConfig.AuditURL)
 		}
 	}
 }
 
-// LogMetricEventToFile writes audit event to local file.
-func (service *AuditService) LogMetricEventToFile(event models.MetricEvent) error {
-	file, err := os.OpenFile(service.serverConfig.AuditFile, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
-	if err != nil {
-		return err
+func logMetricEventsToFile(events chan models.MetricsReceivedEvent, auditFilePath string) {
+	for event := range events {
+		file, err := os.OpenFile(auditFilePath, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+		if err != nil {
+			logger.Sugar.Errorf("metrics event save to file error: %w", err)
+			continue
+		}
+		defer file.Close()
+		encoder := json.NewEncoder(file)
+		if err := encoder.Encode(event); err != nil {
+			logger.Sugar.Errorf("metrics event save to file error: %w", err)
+			continue
+		}
+		timestamp := time.UnixMilli(event.TS).Format("2006-01-02 15:04:05.000")
+		logger.Sugar.Infof("metrics event with TS %s successfully saved in file", timestamp)
 	}
-	defer file.Close()
-	encoder := json.NewEncoder(file)
-	if err := encoder.Encode(event); err != nil {
-		return err
-	}
-	logger.Sugar.Infof("metrics event successfully saved in file")
-	return nil
 }
 
-// LogMetricEventToRemoteService sends audit event to remote service.
-func (service *AuditService) LogMetricEventToRemoteService(event models.MetricEvent) error {
-	var client = resty.New()
-	buf := new(bytes.Buffer)
-	encoder := json.NewEncoder(buf)
-	err := encoder.Encode(event)
-	if err != nil {
-		return err
+func logMetricEventToRemoteService(events chan models.MetricsReceivedEvent, auditURL string) {
+	for event := range events {
+		var client = resty.New()
+		buf := new(bytes.Buffer)
+		encoder := json.NewEncoder(buf)
+		err := encoder.Encode(event)
+		if err != nil {
+			logger.Sugar.Errorf("metrics event send to remote service error: %w", err)
+			continue
+		}
+		request := client.R().
+			SetHeader("Content-Type", "application/json").
+			SetBody(buf.Bytes())
+		resp, err := request.Post(auditURL)
+		if err != nil {
+			logger.Sugar.Errorf("metrics event send to remote service error: %w", err)
+			continue
+		}
+		if resp.StatusCode() != http.StatusOK {
+			err := fmt.Errorf("incorrect response status code: %d", resp.StatusCode())
+			logger.Sugar.Errorf("metrics event send to remote service error: %w", err)
+			continue
+		}
+		timestamp := time.UnixMilli(event.TS).Format("2006-01-02 15:04:05.000")
+		logger.Sugar.Infof("metrics event with TS %s successfully sent to remote service", timestamp)
 	}
-	request := client.R().
-		SetHeader("Content-Type", "application/json").
-		SetBody(buf.Bytes())
-	resp, err := request.Post(service.serverConfig.AuditURL)
-	if err != nil {
-		return err
-	}
-	if resp.StatusCode() != http.StatusOK {
-		return fmt.Errorf("incorrect response status code: %d", resp.StatusCode())
-	}
-	logger.Sugar.Infof("metrics event successfully sent to remote service")
-	return nil
+
 }
