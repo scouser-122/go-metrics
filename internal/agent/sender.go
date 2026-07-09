ea136700fabc8e2a8ab86d1a7d4ad77e79b08ec1
scouser-122 <scouser122@gmail.com>
2026-07-09 13:12:18 +0300

    fix: correct stop sending metrics logic

--- internal/agent/sender.go
+++ internal/agent/sender.go
@@ -101,13 +101,8 @@ func (sender *MetricsSender) SendMetricsContinuousWorker(
 			for data := range dataCh {
 				logger.Sugar.Infof("start sending metrics in worker %d", w)
 				sender.SendMetrics(data.metrics)
-				if len(dataCh) == 0 {
-					timeDiff := time.Since(prevTime)
-					if timeDiff < interval {
-						time.Sleep(interval - timeDiff)
-					}
-				}
-				prevTime = time.Now()
+
+				// stop process if stop signal received
 				select {
 				case <-stopCh:
 					logger.Sugar.Infof("stop sending metrics in worker %d", w)
@@ -115,6 +110,14 @@ func (sender *MetricsSender) SendMetricsContinuousWorker(
 					return
 				default:
 				}
+
+				if len(dataCh) == 0 {
+					timeDiff := time.Since(prevTime)
+					if timeDiff < interval {
+						time.Sleep(interval - timeDiff)
+					}
+				}
+				prevTime = time.Now()
 			}
 		}()
 	}
